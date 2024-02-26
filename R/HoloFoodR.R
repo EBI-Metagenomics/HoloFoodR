106309be92e3e966b37b3a6e733994fe66440eeb
TuomasBorman <tvborm@utu.fi>
2024-02-26 10:31:34 +0200

    Add HoloFoodR file

---
+++ R/HoloFoodR.R
@@ -0,0 +1,16 @@
+#' \code{HoloFoodR} Package.
+#'
+#' \code{HoloFoodR} implements an interface to the EBI HoloFood database.
+#' See the vignette for a general introduction to this package.
+#' [About HoloFood](https://www.holofood.eu/) for general HoloFood
+#' information, and
+#' [API documentation](https://www.holofooddata.org/api/docs) for
+#' details about the JSONAPI implementation.
+#' @name HoloFoodR
+#' @docType package
+#' @seealso \link[SummarizedExperiment:SummarizedExperiment-class]{SummarizedExperiment} class
+NULL
+
+#' @import SummarizedExperiment
+#' @import MultiAssayExperiment
+NULL
