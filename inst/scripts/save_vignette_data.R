7773c0d1922122d6cdf7dba166fa638e63d2c09b
TuomasBorman <tvborm@utu.fi>
2024-06-06 13:43:54 +0300

    up

---
+++ inst/scripts/save_vignette_data.R
@@ -0,0 +1,34 @@
+
+library(HoloFoodR)
+
+animals <- doQuery("animals", max.hits = 100)
+animals <- animals[ animals[["histological"]], ]
+
+animal_data <- getData(
+    accession.type = "animals", accession = animals[["accession"]])
+
+samples <- animal_data[["samples"]]
+
+
+sample_ids <- unique(samples[["accession"]])
+
+mae <- getResult(sample_ids)
+
+library(MGnifyR)
+
+mg <- MgnifyClient(useCache = TRUE)
+
+# Get those samples that are metagenomic samples
+metagenomic_samples <- samples[
+    samples[["sample_type"]] == "metagenomic_assembly", ]
+
+# Get analysis IDs based on sample IDs
+analysis_ids <- searchAnalysis(
+    mg, type = "samples", metagenomic_samples[["accession"]])
+
+path <- system.file("extdata", "analysis_ids.rds", package = "HoloFoodR")
+saveRDS(analysis_ids, path)
+
+mae_metagenomic <- MGnifyR::getResult(mg, analysis_ids)
+path <- system.file("extdata", "mae_metagenomic.rds", package = "HoloFoodR")
+mae_metagenomic <- saveRDS(mae_metagenomic, path)
