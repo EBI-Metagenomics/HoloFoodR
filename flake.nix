2b6bf8994427beea417c224b4ae1d29b3a56c6c9
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-09-13 16:03:36 +0300

    add: flake

--- flake.nix
+++ flake.nix
@@ -1,11 +1,33 @@
 {
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs";
-  };
+    flake-utils.url = "github:numtide/flake-utils";
 
+  };
   outputs =
-    { self, nixpkgs }:
     {
-      packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-    };
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+        renv = with pkgs.rPackages; [
+          dplyr
+        ];
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            R
+            renv
+          ];
+        };
+      }
+    );
 }
