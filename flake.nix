83dd7f6a46a21b099e06a068a07a44246ac62aef
Artur Sannikov <40318410+artur-sannikov@users.noreply.github.com>
2024-10-22 11:03:28 +0300

    change flake.nix

--- flake.nix
+++ flake.nix
@@ -1,34 +1,46 @@
 {
   inputs = {
     nixpkgs.url = "github:rstats-on-nix/nixpkgs/c2a9a8c6b785e80d0188c29f2dc9f1870c95b812";
-    flake-utils.url = "github:numtide/flake-utils";
 
   };
   outputs =
+    { self, nixpkgs }:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+              overlays = [ self.overlays.default ];
+            };
+          }
+        );
+    in
     {
-      self,
-      nixpkgs,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
+      overlays.default = final: prev: rec {
+        rEnv = final.rWrapper.override {
+          packages = with final.rPackages; [ HoloFoodR ];
         };
-        renv = with pkgs.rPackages; [
-          dplyr
-          HoloFoodR
-        ];
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            R
-            renv
-          ];
-        };
-      }
-    );
+      };
+
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              rEnv
+              quarto
+            ];
+          };
+        }
+      );
+    };
 }
